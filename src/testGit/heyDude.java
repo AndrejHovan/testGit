0b8daaba7c3a0954d8b3ed1a8a2d1bd545f00f79
student <student@158.197.35.79>
2018-09-19 07:32:50 +0200

    5x testing
    pridany for cyklus na test

--- src/testGit/heyDude.java
+++ src/testGit/heyDude.java
@@ -5,6 +5,9 @@ public class heyDude {
 	public static void main(String[] args) {
 		
 		System.out.println("Wazzup");
+		for (int i = 0; i < 5; i++) {
+			System.out.println("testing, testing, 1 2 3..");
+		}
 
 	}
 
